7396967be671123ee1fff7a56527cf748d9e6759
Riccardo Gozzovelli <go_riky@hotmail.it>
2018-07-16 15:19:41 +0200

    Immagini aggiunte al momento solo per Android. Il Custom Renderer inserito permette di eliminare la selezione degli Item all'interno di una ListView

---
+++ SkaffolderTemplate/SkaffolderTemplate/CustomRenderer/CustomViewCell.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace SkaffolderTemplate.CustomRenderer
+{
+    public class CustomViewCell : ViewCell
+    {
+        public static readonly BindableProperty TappedCommandProperty =
+               BindableProperty.Create("TappedCommandProperty", typeof(ICommand), typeof(CustomViewCell), null, propertyChanged: OnTappedCommandChanged);
+
+        public ICommand TappedCommand
+        {
+            get { return (ICommand)GetValue(TappedCommandProperty); }
+            set { SetValue(TappedCommandProperty, value); }
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            OnTappedCommandChanged(this, null, null);
+        }
+
+        private static void OnTappedCommandChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            var viewCell = bindable as CustomViewCell;
+
+            if (viewCell == null)
+                return;
+
+            viewCell.View.GestureRecognizers.Clear();
+            viewCell.View.GestureRecognizers.Add(new TapGestureRecognizer() { Command = viewCell.TappedCommand });
+        }
+    }
+}
